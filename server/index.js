6c7c078ce8a32367b991e18c6fcf77160a1a2bc0
avent18 <naveenkumarnghspn@gmail.com>
2026-05-23 22:29:00 +0530

    trailing slash removed

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-    origin:"https://interview-io-sigma.vercel.app/",
+    origin:"https://interview-io-sigma.vercel.app",
     credentials:true
 }))
 
